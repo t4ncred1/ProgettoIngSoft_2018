937337661fa8ab9c5aa26076ca64ea97d6f19222
Tancredi Covioli <10498705@polimi.it>
2018-05-08 22:26:00 +0200

    minor modifications

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,9 +1,4 @@
 package it.polimi.ingsw;
-
-//import junit.framework.Test;
-//import junit.framework.TestSuite;
-//import junit.framework.TestCase;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
