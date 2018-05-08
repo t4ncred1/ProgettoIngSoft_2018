31b339a953a941a2cbe1cc3b26828aa5d0902eca
Tancredi Covioli <10498705@polimi.it>
2018-05-08 22:25:26 +0200

    modified BoxTest with jupiter APIs

--- src/test/java/it/polimi/ingsw/BoxTest.java
+++ src/test/java/it/polimi/ingsw/BoxTest.java
@@ -1,40 +1,48 @@
 package it.polimi.ingsw;
 
 import it.polimi.ingsw.customException.NotProperParameterException;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BoxTest {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
 //Tests sui costruttori
     @Test
     public void boxPositions() throws NotProperParameterException{
-        thrown.expect(NotProperParameterException.class);
-        Box test_Box = new Box(9, 10);
+//        thrown.expect(NotProperParameterException.class);
+        assertThrows(NotProperParameterException.class,
+                () ->{
+                Box test_Box = new Box(9, 10);
+                });
     }
     @Test
     public void boxValue() throws NotProperParameterException{
-        thrown.expect(NotProperParameterException.class);
-        Box test_box = new Box(9,0,0);
+        assertThrows(NotProperParameterException.class,
+                () -> {
+                    Box test_box = new Box(9, 0, 0);
+                });
     }
     @Test
     public void boxColor() throws NotProperParameterException{
-        thrown.expect(NotProperParameterException.class);
-        Box test_box = new Box("ciao",0,0);
+        assertThrows(NotProperParameterException.class,
+                () -> {
+                    Box test_box = new Box("ciao",0,0);
+                });
     }
 
 //Test sulla funzione update
     @Test
     public void update_Test() throws NotProperParameterException{
-        thrown.expect(NotProperParameterException.class);
+
         Box test_box = new Box(0,0);
-        test_box.update(false, new DieToCostraintsAdapter(new Die("Red",4)), 9, 6);
+        assertThrows(NotProperParameterException.class,
+                () -> {
+                    test_box.update(false, new DieToCostraintsAdapter(new Die("Red",4)), 9, 6);
+                });
+
     }
 
     /*----------------------------------------------------------------------------------------------------*/
