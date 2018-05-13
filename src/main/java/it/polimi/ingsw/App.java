68ce0678bebf4173796eaa5fe995f9af0e602f0b
Tancredi Covioli <10498705@polimi.it>
2018-05-14 00:31:00 +0200

    general updates

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,5 +1,11 @@
 package it.polimi.ingsw;
 
+import com.google.gson.Gson;
+import it.polimi.ingsw.custom_exception.NotValidParameterException;
+import it.polimi.ingsw.grid_container.Grid;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 /**
