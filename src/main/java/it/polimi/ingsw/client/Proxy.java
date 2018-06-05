f28c58cde51be1cbba6424ba7e4b7ed14a9536e3
polimi-hyp-2018-10497583 <andrea1.costanzo@mail.polimi.it>
2018-06-05 16:20:38 +0200

    Added methods in Proxy to: * set dicePool * update selectedGrid * get selected grid * locally try a die insertion (a control by the server will be executed   anyway)

--- src/main/java/it/polimi/ingsw/client/Proxy.java
+++ src/main/java/it/polimi/ingsw/client/Proxy.java
@@ -1,8 +1,12 @@
 package it.polimi.ingsw.client;
 
+import it.polimi.ingsw.client.custom_exception.InvalidMoveException;
+import it.polimi.ingsw.server.components.Die;
 import it.polimi.ingsw.server.components.Grid;
 import it.polimi.ingsw.server.custom_exception.InvalidOperationException;
+import it.polimi.ingsw.server.custom_exception.NotValidParameterException;
 
+import java.io.DataInputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,7 +17,9 @@ public class Proxy {
     private static Proxy instance;
 
     private ArrayList<Grid> gridsSelection;
+    private Grid gridSelected;
     private Map<String,Grid> playerGrids;
+    private ArrayList<Die> dicePool;
 
     private Proxy(){
         gridsSelection= new ArrayList<>();
@@ -36,5 +42,34 @@ public class Proxy {
 
     public int getGridsSelectionDimension() {
         return gridsSelection.size();
+    }
+
+    public void tryToInsertDieInXY(int position, int x, int y) throws InvalidMoveException {
+        final boolean checkColorConstraint= true;
+        final boolean checkValueConstraint= true;
+        Die die=null;
+        //FIXME here should be launched two different exceptions for dicePool and insertDie
+        try {
+            dicePool.remove(position);
+        }catch (IndexOutOfBoundsException e){
+            throw new InvalidMoveException();
+        }
+        try {
+            gridSelected.insertDieInXY(x, y, checkColorConstraint, checkValueConstraint, die);
+        } catch (Exception e) {
+            throw new InvalidMoveException();
+        }
+    }
+
+    public void updateGridSelected(Grid grid) {
+        this.gridSelected=grid;
+    }
+
+    public Grid getGridSelected() {
+        return this.gridSelected;
+    }
+
+    public void setDicePool(List<Die> dicePool) {
+        this.dicePool=(ArrayList<Die>) dicePool;
     }
 }
