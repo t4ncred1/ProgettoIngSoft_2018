71c576f2b5f0ecc824fdf3b16a118795b2bed238
t4ncred1 <tancredi.covioli@mail.polimi.it>
2018-07-02 01:54:13 +0200

    made javadocs for PublicObjective class.

--- src/main/java/it/polimi/ingsw/server/model/cards/PublicObjective.java
+++ src/main/java/it/polimi/ingsw/server/model/cards/PublicObjective.java
@@ -8,6 +8,14 @@ public class PublicObjective extends Objective{
     private int value;
     private int cardNumber;
 
+    /**
+     * Constructor for PublicObjective.
+     * Note: this method is not used except in tests.
+     * @param tit the public objective's title.
+     * @param desc the public objective's description.
+     * @param card the public objective's card number.
+     * @param points the number of points assigned to player when calculatePoints is called.
+     */
     public PublicObjective(String tit, String desc, int card, int points){
         title=tit;
         description=desc;
@@ -15,7 +23,10 @@ public class PublicObjective extends Objective{
         value=points;
     }
 
-
+    /**
+     *
+     * @return A string containing the current values of the fields Description, CardNumber and Value, formatted nicely.
+     */
     public String toString(){
         StringBuilder build = new StringBuilder("Title = ");
         build.append(this.getTitle());
@@ -41,7 +52,7 @@ public class PublicObjective extends Objective{
         switch (cardNumber) {
 
             case 1:
-                actualGrid = rotatedGrid(grid);
+                actualGrid = symmetricGrid(grid);
 
             case 2:
                 returnValue = 0;
@@ -64,7 +75,7 @@ public class PublicObjective extends Objective{
                 break;
 
             case 3:
-                actualGrid = rotatedGrid(grid);
+                actualGrid = symmetricGrid(grid);
 
             case 4:
                 returnValue = 0;
@@ -98,11 +109,8 @@ public class PublicObjective extends Objective{
                 returnValue=findArraysInGrid(grid.getGrid(), new int[]{1,2,3,4,5,6})*value;
                 break;
             case 9:
-                actualGrid = diagonalGridRTL(grid);
                 boolean[][] alreadyUsed = new boolean[grid.getColumnNumber()][grid.getRowNumber()];
-                returnValue = cicleThroughDiagonalgrid(alreadyUsed, returnValue, actualGrid);
-                actualGrid = diagonalGridLTR(grid);
-                returnValue = cicleThroughDiagonalgrid(alreadyUsed, returnValue, actualGrid);
+                returnValue = cicleThroughDiagonalgrid(alreadyUsed, diagonalGridLTR(grid)) + cicleThroughDiagonalgrid(alreadyUsed, diagonalGridRTL(grid));
                 break;
             default: // case 10
                 returnValue=findColorArraysInGrid(grid.getGrid(), new int[]{0,1,2,3,4})*value;
@@ -110,6 +118,14 @@ public class PublicObjective extends Objective{
         }
         return returnValue;
     }
+
+    /**
+     * Used in CalculatePoints for Public Objective 10.
+     * @param actualGrid grid on which the groups are counted.
+     * @param colorConstraintsToCheck constraint indexes of the colors that must be grouped.
+     * @return The number of groups (not interwoven) of colors specified in colorConstraintToCheck.
+     * @see it.polimi.ingsw.server.model.components.DieToConstraintsAdapter to get the association between constraintIndexes and colors.
+     */
 
     private int findColorArraysInGrid( Box[][] actualGrid, int[] colorConstraintsToCheck){
         boolean[][] alreadyUsed = new boolean[actualGrid.length][actualGrid[0].length]; //here we assume the matrix is rectangular
@@ -134,6 +150,14 @@ public class PublicObjective extends Objective{
         return returnValue;
     }
 
+    /**
+     * Method only called by findColorArraysInGrid.
+     * @param alreadyUsed matrix representing the boxes already checked to find color groups.
+     * @param actualGrid grid from which the results are given.
+     * @param colorConstraints constraint indexes of the colors that must be grouped.
+     * @return true if, between the boxes not already checked, there's a group of the same colors given by colorConstraints.
+     * @see #findSingleColorArrayInGrid(boolean[][], Box[][], int[])
+     */
     private boolean findSingleColorArrayInGrid(boolean[][] alreadyUsed, Box[][] actualGrid, int[] colorConstraints){
         for(int i=0; i<actualGrid.length;i++) {
             for (int j = 0; j < actualGrid[0].length; j++) {
@@ -153,6 +177,12 @@ public class PublicObjective extends Objective{
         return false;
     }
 
+    /**
+     *  Used in CalculatePoints for Public Objective 5,6,7,8.
+     * @param actualGrid grid from which the results are given.
+     * @param valuesToCheck array of integers that the method will try to find in actualGrid and group.
+     * @return number of groups of numbers in valuesToCheck found in actualGrid.
+     */
     private int findArraysInGrid( Box[][] actualGrid, int[] valuesToCheck){
         boolean[][] alreadyUsed = new boolean[actualGrid.length][actualGrid[0].length]; //here we assume the matrix is rectangular
         int returnValue=0;
@@ -177,6 +207,14 @@ public class PublicObjective extends Objective{
 
     }
 
+    /**
+     * Method only used by findArraysInGrid.
+     * @param alreadyUsed matrix representing the boxes already checked to find number groups.
+     * @param actualGrid grid from which the results are given.
+     * @param valuesToCheck array of integers that the method will try to find in actualGrid and group.
+     * @return true if, between the boxes not already checked, there's a group of the same numbers given by valuesToCheck.
+     * @see #findArraysInGrid(Box[][], int[])
+     */
     private boolean findSingleArrayInGrid(boolean[][] alreadyUsed, Box[][] actualGrid, int[] valuesToCheck){
         for(int i=0; i<actualGrid.length;i++) {
             for (int j = 0; j < actualGrid[0].length; j++) {
@@ -196,9 +234,17 @@ public class PublicObjective extends Objective{
         return false;
     }
 
-    private int cicleThroughDiagonalgrid(boolean[][] alreadyUsed, int returnValue, Box[][] actualGrid){
+    /**
+     * Used in calculatePoints for public objective 9.
+     * @param alreadyUsed matrix rapresenting already checked boxes.
+     * @param actualGrid original Box matrix on which the method is applied.
+     * @return number of same color boxes on the same column
+     *
+     */
+    private int cicleThroughDiagonalgrid(boolean[][] alreadyUsed, Box[][] actualGrid){
         int previousColor;
         int currentColor;
+        int returnValue=0;
         boolean second = true;
         for (Box[] i : actualGrid){
             previousColor=-1;
@@ -225,7 +271,12 @@ public class PublicObjective extends Objective{
         return returnValue;
     }
 
-    private Box[][] rotatedGrid(Grid grid){
+    /**
+     * Used to create a copy of the grid reflected on its diagonal.
+     * @param grid grid to reflect.
+     * @return Array of boxes representing the grid passed reflectd on its upper left to lower right axis.
+     */
+    private Box[][] symmetricGrid(Grid grid){
         Box[][] rotatedGrid = new Box[grid.getRowNumber()][grid.getColumnNumber()];
         for (int i=0;i<grid.getColumnNumber();i++){
             for(int j=0;j<grid.getRowNumber();j++){
@@ -235,6 +286,10 @@ public class PublicObjective extends Objective{
         return rotatedGrid;
     }
 
+    /**
+     * @param grid grid analyzed to get the resulting grid.
+     * @return a Box matrix representing the grid passed as parameter but modified to have, on its columns, the left to right diagonals of the original grid.
+     */
     private Box[][] diagonalGridLTR(Grid grid){
         Box[][] actualGrid = grid.getGrid();
         Box[][] diagonalGrid = new Box[grid.getColumnNumber()+grid.getRowNumber()-1][];
@@ -253,6 +308,11 @@ public class PublicObjective extends Objective{
         }
         return diagonalGrid;
     }
+
+    /**
+     * @param grid grid analyzed to get the resulting grid.
+     * @return a Box matrix representing the grid passed as parameter but modified to have, on its columns, the right to left diagonals of the original grid.
+     */
 
     private Box[][] diagonalGridRTL(Grid grid){
         Box[][] actualGrid = grid.getGrid();
